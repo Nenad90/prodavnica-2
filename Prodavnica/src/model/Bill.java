c62346b5ab52ab0b0c4cc856b3e221720e8f42cb
nenad <ja@gmail.com>
2019-04-17 21:50:25 +0200

    registracija sredjena

--- Prodavnica/src/model/Bill.java
+++ Prodavnica/src/model/Bill.java
@@ -7,6 +7,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 
 @Entity
 public class Bill {
@@ -14,7 +16,9 @@ public class Bill {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int idBill;
+	@OneToOne
 	private User user;
+	@OneToMany
 	private List<Product> listOfProducts = new ArrayList<Product>();
 	private double totalPrice;
 	
