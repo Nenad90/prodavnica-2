37d502f6b3f25b7fbb61cee68b6e33ab2f7600ad
nenad <ja@gmail.com>
2019-05-06 21:53:05 +0200

    admin razradjen malo

--- Prodavnica/src/controler/LoginServlet.java
+++ Prodavnica/src/controler/LoginServlet.java
@@ -6,8 +6,11 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import bussinesService.LoginMetode;
+import model.Admin;
+import model.User;
 
 /**
  * Servlet implementation class LoginServlet
@@ -29,10 +32,22 @@ public class LoginServlet extends HttpServlet {
 		if(metode.daLiPostojiUser(userName)) {
 			
 			if(metode.daLiJeDobarPassword(userName, password)) {
-				if(metode.vratiUsera(userName, password) != null) {
-					//nastavljam
+				User user = metode.vratiUsera(userName, password);
+				if( user!= null) {
+					
+					HttpSession sesija = request.getSession();
+					sesija.setAttribute("ovdeCuvamUsera", user);
+					
+					Admin admin = new Admin();
+					
+					if(userName.equals(admin.getAdminUserName()) && password.equals(admin.getAdminPassword())) {
+						response.sendRedirect("jsp/admin.jsp");
+					}else {
+						response.sendRedirect("jsp/user.jsp");
+					}
+					
 				}else {
-					response.sendRedirect("error.html");
+					response.sendRedirect("loginError.html");
 				}
 			}else {
 				response.sendRedirect("loginError.html");
